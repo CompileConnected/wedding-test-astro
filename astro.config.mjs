45fb291290b2967fcafd8f187224963b36f1e3e8
Yohanes <yokho95@gmail.com>
2023-07-19 23:02:22 +0700

    add image gallery, add vue as image gallery handle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import lightningcss from 'vite-plugin-lightningcss';
 import vercel from '@astrojs/vercel/serverless';
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), vue()],
+  vite: {
+    plugins: [
+      lightningcss({
+        browserslist: '>= 0.25%',
+      }),
+    ]
+  },
   output: "server",
   adapter: vercel({
-    analytics: true,
-  }),
+    analytics: true
+  })
 });
