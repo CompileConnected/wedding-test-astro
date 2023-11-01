82b4a05ca0c6de50ad931417ed26a5f3a8b58c31
Yohanes <yokho95@gmail.com>
2023-11-01 12:09:10 +0700

    update to english text

--- src/components/OpenInvitationButton.svelte
+++ src/components/OpenInvitationButton.svelte
@@ -10,7 +10,7 @@
 	}}
 	>
 	<span class="flex items-center h-full w-full justify-center gap-1 flex-nowrap whitespace-nowrap">
-		<iconify-icon icon="mdi:email" width="24" height="24"></iconify-icon>  Buka Undangan
+		<iconify-icon icon="mdi:email" width="24" height="24"></iconify-icon>  Open
 		<div class="h-2 w-2 relative -mr-2 ml-2">
 			<div
 				class="absolute left-0 top-0 transition-all duration-300 bg-white group-hover:bg-black w-2 h-2 rounded-full"
